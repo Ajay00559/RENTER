1dfdd009ff53221d676e3a9d76d0f36eabcf8cb7
Ajay00559 <patelajay00559@gmail.com>
2023-02-26 01:57:15 +0530

    initial

--- script.js
+++ script.js
@@ -5,6 +5,11 @@
 //           })
 //  }
 //  navanimation()
+
+// document.images.ready.then(function(){
+//  loaderanimation()
+    
+// });
 function menuToggle()
  {
     var border=document.querySelector('#border-bottom');
@@ -41,20 +46,80 @@ navlinks.forEach(function(elem){
         })
         tlLoader.to('.loader',{
             opacity:0,
-            height:"-500%",
+            height:"0%",
             duration:2,
             delay:-1,
             ease:Expo.easeInOut,
-            onComplete: function(){
-                landinganimation()
-
-            }
+            
     
         })
     
  }
 
- loaderanimation()
+//  loaderanimation()
+loaderanimation()
+
+function animhouse(){
+    const canvas = document.querySelector(".canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    
+    const context = canvas.getContext("2d");
+    const frameCount = 195;
+    
+    const currentFrame = (index) => `./3dhousepngimages/${(index + 1).toString()}.png`;
+    
+    const images = [];
+    let house = { frame: 0 };
+    
+    for (let i = 0; i < frameCount; i++) {
+      const img = new Image();
+      img.src = currentFrame(i);
+      console.log(currentFrame(i));
+      images.push(img);
+    }
+    
+    gsap.to(house, {
+      frame: frameCount - 1,
+      snap: "frame",
+      ease: "none",
+      scrollTrigger: {
+        scrub: 0.5,
+        start:"top 10%",
+        markers:true,
+        pin: "canvas",
+        end: "500%",
+      },
+      onUpdate: render,
+    });
+    
+     
+    
+    images[0].onload = render;
+    
+    function render() {
+      context.canvas.width = images[0].width;
+      context.canvas.height = images[0].height;
+    
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.drawImage(images[house.frame], 0, 0);
+    }
+    
+}
+animhouse()
+
+gsap.to('.canvas',{
+    scrollTrigger:{
+        trigger:'#landing-page',
+        start:'top 20%',
+        end:'top 20%',
+
+    },
+    onComplete: function (){
+        landinganimation()
+    }
+
+})
 
  function landinganimation(){
     var txt
